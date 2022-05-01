fd856ffc64dbde830e505b31297860975b79f3e8
ParkerEM <eparker00@pm.me>
2022-05-01 11:07:06 -0400

    render update

--- hello-world/src/HelloWorld.js
+++ hello-world/src/HelloWorld.js
@@ -22,24 +22,26 @@ export class HelloWorld extends LitElement {
     `;
   }
 
+  // define properties
   constructor() {
     super();
-    // define properties
     this.greeting = 'Hello ';
     this.planet = 'World';
   }
 
-  // renders planet field on click
+  // renders greeting and planet field, toggle on click
   render() {
     return html`
-      <span onclick="${this.toggleAttribute}">${this.greeting}<span class="planet">${this.planet}</span></span>
+      <span onclick="${this.toggleAttribute}">${this.greeting}
+      <span class="planet">${this.planet}</span></span>
     `;
   }
 
-  // update the render of planet field
+  // switch between planet field
   toggleAttribute() {
     this.planet = this.planet === 'World' ? 'Neptune' : 'World';
   }
 }
 
 customElements.define('hello-world', HelloWorld);
+
